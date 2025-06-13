8ddae7963d0c6055e55112b9d04802429cf5cef6
sem pruijs <sem@pruijs.net>
2025-06-13 17:20:49 +0200

    put sidebar on the left on content on the right

--- site/src/routes/[book]/[chapter]/+page.svelte
+++ site/src/routes/[book]/[chapter]/+page.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
-  import { Option } from "effect";
   import { goto } from "$app/navigation";
-  import { error } from '@sveltejs/kit';
   import type { Bible, Book, Chapter } from '$lib/types';
   import bibleData from '$lib/translations/stv.json';
   import Sidebar from "$lib/components/Sidebar.svelte";
@@ -13,12 +11,14 @@
 	let { data }: PageProps = $props();
 </script>
 
-<div class="">
-  <Sidebar {bible} />
-{#if data.chapter}
-  <button onclick={() => goto('/genesis/1')}>Ga naar genesis</button>
-  <ChapterDetail chapter={data.chapter} />
-{:else}
-  <h1>could not fin dbook</h1>
-{/if}
+<div class="flex h-screen">
+  <Sidebar {bible} class="w-64 border-r border-gray-200 p-4" />
+
+  <div class="flex-1 p-6 overflow-auto">
+    {#if data.chapter}
+      <ChapterDetail chapter={data.chapter} />
+    {:else}
+      <h1>Could not find book</h1>
+    {/if}
+  </div>
 </div>
