c42216c378556085f8e4b1e01299d0d85fe7b81f
sem pruijs <sem@pruijs.net>
2025-06-14 18:29:00 +0200

    add favicons

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -4,6 +4,7 @@ import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
@@ -15,5 +16,40 @@ export default defineConfig({
       globals: { Buffer: true, global: true },
       protocolImports: true,
     }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.png', 'icon-512.png', 'icon-192.png', 'icon-180.png'],
+      manifest: {
+        name: 'Pure bible',
+        short_name: 'Bible',
+        description: 'Fast and open source bible',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-180.png',
+            sizes: '180x180',
+            type: 'image/png'
+          },
+          {
+            src: '/favicon.png',
+            sizes: '128x128',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
 });
