fb3fb6fd56b4150d39738a50d8ec225dbf5d808d
sem pruijs <sem@pruijs.net>
2025-06-15 18:35:45 +0200

    add verse numbers

--- site/src/lib/components/ChapterDetail.svelte
+++ site/src/lib/components/ChapterDetail.svelte
@@ -12,6 +12,9 @@
 <p class="mb-40 text-xl leading-relaxed">
   {#each chapter.verses as verse, i}
     <span class="inline">
+      <span class="text-gray-500 text-sm align-super pr-1">
+        {verse.verse}
+      </span>
       {verse.text}{i < chapter.verses.length - 1 ? ' ' : ''}
     </span>
   {/each}
