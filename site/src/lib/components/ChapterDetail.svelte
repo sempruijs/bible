086103b31c50f928e8a0f491164964da5614622b
sem pruijs <sem@pruijs.net>
2025-06-15 20:24:39 +0200

    Improve chapter detail view

--- site/src/lib/components/ChapterDetail.svelte
+++ site/src/lib/components/ChapterDetail.svelte
@@ -10,12 +10,14 @@
 <!-- One continuous paragraph of text -->
 <div class="grid place-items-center min-h-screen px-4 text-center">
 <h1 class="text-3xl font-bold mb-6">{chapter.name}</h1>
-<p class="mb-40 text-lg leading-relaxed max-w-prose mx-auto ml-20 mr-20">
+<p class="mb-40 text-lg leading-relaxed max-w-prose mx-auto text-left ml-20 mr-20">
   {#each chapter.verses as verse, i}
     <span class="inline">
-      <span class="text-gray-500 text-sm align-super pr-1">
+      {#if verse.verse != 1}
+      <span class="text-gray-500 text-sm align-super -pr-1 pl-1">
         {verse.verse}
       </span>
+      {/if}
       {verse.text}{i < chapter.verses.length - 1 ? ' ' : ''}
     </span>
   {/each}
