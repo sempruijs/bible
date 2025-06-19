477befabef1f2fb3c3a3429240dc85a6c7788942
sem pruijs <sem@pruijs.net>
2025-06-19 17:01:38 +0200

    finish next previous buttons

--- site/src/lib/components/ChapterDetail.svelte
+++ site/src/lib/components/ChapterDetail.svelte
@@ -10,7 +10,7 @@
 <!-- One continuous paragraph of text -->
 <div class="grid place-items-center px-4 text-center">
 <h1 class="text-3xl font-bold mb-6">{chapter.name}</h1>
-<p class="mb-40 text-lg leading-relaxed max-w-prose mx-auto text-left ml-20 mr-20">
+<p class="mb-10 text-lg leading-relaxed max-w-prose mx-auto text-left ml-20 mr-20">
   {#each chapter.verses as verse, i}
     <span class="inline">
       {#if verse.verse != 1}
