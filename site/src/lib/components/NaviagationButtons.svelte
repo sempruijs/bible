f30e651c1c1b659cdeaaee72fe7b35f95a2bf98f
sem pruijs <sem@pruijs.net>
2025-06-19 14:50:43 +0200

    add arrow key navigation

--- site/src/lib/components/NaviagationButtons.svelte
+++ site/src/lib/components/NaviagationButtons.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { Option } from "effect";
+  import { onMount } from "svelte";
   import type { Bible, Chapter, Book } from "$lib/types";
   import { getSafe, findSafe } from "$lib/getSave";
   import { chapterToPath } from "$lib/types";
@@ -20,6 +21,25 @@
     next = Option.flatMap(index, (i) => getSafe(allChapters, i + 1));
     previous = Option.flatMap(index, (i) => getSafe(allChapters, i - 1));
   });
+
+  $effect(() => {
+    const onKeyDown = (e: KeyboardEvent) => {
+      if (e.metaKey || e.ctrlKey || e.altKey || e.shiftKey) {
+        return;
+      }
+
+      if (e.key === "ArrowRight" && Option.isSome(next)) {
+        const path = chapterToPath(next.value);
+        goto(path);
+      } else if (e.key === "ArrowLeft" && Option.isSome(previous)) {
+        const path = chapterToPath(previous.value);
+        goto(path);
+      }
+  	}
+
+    window.addEventListener("keydown", onKeyDown);
+    return () => window.removeEventListener("keydown", onKeyDown);
+	});
 </script>
 
 {#if Option.isSome(previous)}
