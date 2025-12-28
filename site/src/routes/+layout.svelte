3c6220952c08f7a2888f4188db9123f7cb3b4da7
Sem Pruijs <sem@pruijs.net>
2025-12-28 22:49:21 +0100

    Create an loadBibleData effect

--- site/src/routes/+layout.svelte
+++ site/src/routes/+layout.svelte
@@ -2,13 +2,8 @@
 	import "../app.css";
 	import { Effect } from "effect";
 	import { onMount } from "svelte";
-	import { loadBibleData } from "$lib/stores/bible";
-	
+
 	let { children } = $props();
-	
-	onMount(() => {
-		loadBibleData();
-	});
 </script>
 
 <div class="min-h-screen bg-[#0a2540] text-white">
