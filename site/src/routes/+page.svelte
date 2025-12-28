840822f5217924e494c148d040d0a8937b0a1073
Sem Pruijs <sem@pruijs.net>
2025-12-28 22:54:16 +0100

    display first verse of the bible

--- site/src/routes/+page.svelte
+++ site/src/routes/+page.svelte
@@ -1,14 +1,26 @@
 <script lang="ts">
-    import { Effect } from "effect";
+    import { Effect, Option } from "effect";
     import { loadBibleData } from "$lib/translations/loadBibleData";
+    import type { TranslationV0 } from "$lib/translations/v0";
     import { onMount } from "svelte";
+
+    let bibleData: Option.Option<TranslationV0> = Option.none();
 
     onMount(() => {
         Effect.runPromise(loadBibleData()).then(
-            (data) => console.log("Bible data loaded:", data),
+            (data) => {
+                console.log("Bible data loaded:", data);
+                bibleData = Option.some(data);
+            },
             (error) => console.error("Failed to load Bible data:", error),
         );
     });
 </script>
 
 <h1>Bible Computer</h1>
+
+{#if Option.isSome(bibleData)}
+    <p>{bibleData.value.books[0].chapters[0].verses[0].text}</p>
+{:else}
+    <p>loading...</p>
+{/if}
