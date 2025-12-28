b605fdd84c4dd57a3c241fc1b1e7e58ab519601b
Sem Pruijs <sem@pruijs.net>
2025-12-28 23:43:41 +0100

    show entire chapter

--- site/src/routes/[book]/[chapter]/+page.svelte
+++ site/src/routes/[book]/[chapter]/+page.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
+    import { getChapter } from "$lib/translations/translation";
     import type { PageProps } from "./$types";
 
     let { data }: PageProps = $props();
 </script>
 
-<h1>{data.chapter.verses[0].text}</h1>
+<h2>{data.chapter.name}</h2>
+{#each data.chapter.verses as verse}
+    <div>
+        <span>{verse.verse}</span>
+        <span>{verse.text}</span>
+    </div>
+{/each}
