20d8906886c34b5a71d416347eab115414ba8934
Sem Pruijs <sem@pruijs.net>
2025-12-31 16:53:03 +0100

    Make selected chapter blue in CanonExplorer

--- site/src/routes/+layout.svelte
+++ site/src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import "../app.css";
 	import { Effect } from "effect";
 	import { onMount } from "svelte";
+	import { page } from "$app/stores";
 	import CanonExplorer from "$lib/components/CanonExplorer.svelte";
 	import type { LayoutData } from "./$types";
 
@@ -13,7 +14,7 @@
 	<div class="w-80 bg-gray-800 border-r border-gray-700 h-full overflow-y-auto flex-shrink-0">
 		<div class="p-4">
 			<h1 class="text-xl font-bold mb-4 text-gray-100">Bible Computer</h1>
-			<CanonExplorer translation={data.bibleData} />
+			<CanonExplorer translation={data.bibleData} currentPath={$page.url.pathname} />
 		</div>
 	</div>
 	
